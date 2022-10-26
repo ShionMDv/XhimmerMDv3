fefa26b65f6ea9113f92986459bc593f1365a99e
RadjaNine <radjagamer009@gmail.com>
2022-10-26 09:00:35 +0700

    bot

---
+++ plugins/_respontoxic.js
@@ -0,0 +1,75 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+import moment from 'moment-timezone'
+let handler = async (m, { conn, args, command }) => {
+  let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let who = m.sender
+const time = moment.tz('Asia/Jakarta').format('HH')
+  const ultah = new Date('November 4 2022 00:00:01')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+  let name = await conn.getName(m.sender)
+  let runnya = `━━━ꕥ〔 *SOURCE CODE* 〕ꕥ━⬣
+✾ *Script By King Of Bear*
+
+✾ *OFFICIAL GROUP*
+_https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL_
+
+✾ *YOUTUBE OFFICIAL*
+https://youtu.be/iAN51m0-Pyg
+━━━━━━ꕥ`
+  let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+ await conn.sendButton(m.chat, runnya,wm + '\n\n' + botdate, intro, [['🧑OWNER','.owner']], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+            }
+        }
+    })
+}
+
+
+handler.help = ['sc', 'sewasc']
+handler.tags = ['info', 'main']
+
+handler.command = /^(sc|script|sewasc)$/i
+
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+export default handler 
+
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
---
+++ plugins/downloader-tiktok.js
@@ -0,0 +1,26 @@
+import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+  let bearr = 'danzz'
+  let sayang = await fetch(`https://saipulanuar.ga/api/download/tiktokview?apikey=${bearr}&url=${args[0]}`)
+  let json = await sayang.json()
+    if (sayang.status !== 200) throw await sayang.text()
+    if (!json.status) throw json  
+  const { author: { nickname }, video, description } = await tiktokdl(args[0])
+        .catch(async _ => await tiktokdlv2(args[0]))
+        .catch(async _ => await tiktokdlv3(args[0]))
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
+    if (!url) throw 'Can\'t download video!'
+    conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴛᴀɴɢɢᴀʟ ᴅɪᴘᴏꜱᴛɪɴɢ: ${json.result.createdAt}\n➔ ᴅɪ ꜱʜᴀʀᴇꜱ ${json.result.shareCount} ᴋᴀʟɪ\n➔ ᴊᴜᴍʟᴀʜ ʟɪᴋᴇ: ${json.result.likesCount}\n➔ ᴊᴜᴍʟᴀʜ ᴋᴏᴍᴇɴᴛᴀʀ: ${json.result.commentCount}\n➔ ᴊᴜᴍʟᴀʜ ᴠɪᴇᴡ: ${json.result.playCount} ᴋᴀʟɪ ᴅɪᴛᴏɴᴛᴏɴ\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
+        url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
+        ['ᴅᴏɴᴀꜱɪ', `.donasi`],
+        ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`],
+        [null, null]], m)
+}
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
+
+export default handler
