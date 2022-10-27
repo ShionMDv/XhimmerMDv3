a4eed37560fcafe3d7ee74e6371b66e8cbbd2bbe
Radja <86454496+RadjaNine@users.noreply.github.com>
2022-10-28 06:39:15 +0700

    Update and rename plugins/payment.js to plugin/payment.js

--- plugin/payment.js
+++ plugin/payment.js
@@ -12,12 +12,12 @@ let handler = async (m, { conn, usedPrefix }) => {
   let vn = `https://github.com/saipulanuar/Api-Github/raw/main/audio/Donasiku.mp3`
   conn.sendButton(m.chat, `
 ┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
-┃✾ Gopay  [088279268363]
-┃✾ Dana  [088279268363]
-┃✾ Saweria  [https://saweria.co/raraharsita2]
+┃✾ Gopay  [082142961010]
+┃✾ Dana  [085330761834]
+┃✾ Pulsa [085330761834]
 ┗━━━━ꕥ
 ┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
-┃ Ingin donasi? Wa.me/6288279268363
+┃ Ingin donasi? Wa.me/6282142961010
 ┃ _Hasil donasi akan digunakan buat sewa_
 ┃ _atau beli *RDP/VPS* agar bot bisa jalan_
 ┃ _24jam tanpa kendala_
