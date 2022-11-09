9850734b60d09f343d8dbdf52d32c0507b71d947
Shioo <110561925+ShionMDv@users.noreply.github.com>
2022-11-09 12:50:24 +0700

    Add files via upload

---
+++ plugins/_anti+212.js
@@ -0,0 +1,144 @@
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+    clearTimeout(this)
+    resolve()
+}, ms))
+
+let handler = m => m
+
+handler.before = async function (m) {
+   if (m.sender.startsWith('212' || '212')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   if (m.sender.startsWith('92' || '92')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   if (m.sender.startsWith('967' || '967')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+    if (m.sender.startsWith('265' || '265')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+    if (m.sender.startsWith('93' || '93')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+    if (m.sender.startsWith('7' || '7')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+    if (m.sender.startsWith('689' || '689')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+    if (m.sender.startsWith('999' || '999')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('44' || '44')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('1' || '1')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   if (m.sender.startsWith('888' || '888')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('61' || '61')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('66' || '66')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('84' || '84')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('880' || '880')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('91' || '91')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('673' || '673')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('675' || '675')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   } 
+   if (m.sender.startsWith('687' || '687')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+if (m.sender.startsWith('966' || '966')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   /*if (m.sender.startsWith('60' || '60')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }*/ 
+   if (m.sender.startsWith('249' || '249')) {
+conn.sendMessage(m.chat, {text: 'Maaf.. Selain nomor +62, nomor lainnya akan di banned dan jika bot admin akan di kick..\n*_BOT Special Untuk Pengguna Nomor Indonesia!!_*'}, {quoted: m})
+await delay(1000)
+conn.updateBlockStatus(m.sender, "block")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+    }
+
+export default handler
